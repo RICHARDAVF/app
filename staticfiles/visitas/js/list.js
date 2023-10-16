b71845f80b93617d0ccc58a5800a907d8e15706b
RICHARDAVF <richarferro1308@gmail.com>
2023-10-16 10:12:40 -0500

    se corrigio scroll en visitas

--- staticfiles/visitas/js/list.js
+++ staticfiles/visitas/js/list.js
@@ -3,11 +3,11 @@ $(function () {
         language: {
             url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json',
         },
-        responsive: false,
-        autoWidth: false,
-        destroy: true,
-        deferRender: true,
-      
+        // responsive: false,
+        // autoWidth: false,
+        // destroy: true,
+        // deferRender: true,
+        scrollX:true,
         dom:'Qlfrtip',
         fixedColumns:{
             left:3,
